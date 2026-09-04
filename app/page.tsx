a9e46ea0175a22b86c7c668973cdbfeb9f51cc43
douguitarman88 <douguitarman@yahoo.com>
2026-09-03 19:28:38 -0700

    Changed video attributes to get the video to autoplay.

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <>
       <section>
-        <video className="css-v2kfba" preload="auto" autoPlay={true} muted={false} loop={true} playsInline>
+        <video className="css-v2kfba" autoPlay muted loop playsInline preload="auto">
           <source src="https://b8yf6gxfhgurbkvz.public.blob.vercel-storage.com/8513593-uhd_3840_2160_25fps.mp4" type="video/mp4" /> {/* https://www.pexels.com/search/videos/musician/?orientation=landscape */}
         </video>
       </section>
